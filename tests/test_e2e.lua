c5548b86b4afba7b19205089cbc5d12a788f232a
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-08-23 17:56:20 +0000

    tests: implement await cases with child.lua_func helpers

--- tests/test_e2e.lua
+++ tests/test_e2e.lua
@@ -3,10 +3,27 @@ local eq = MiniTest.expect.equality
 
 local child = MiniTest.new_child_neovim()
 
+local await_value = function(...)
+  return child.lua_func(function(...)
+    local result = {}
+    local done = false
+    local args = { ..., }
+
+    coroutine.wrap(function()
+      result = { M.await(function(resolve) resolve(unpack(args)) end), }
+      done = true
+    end)()
+
+    vim.wait(10, function() return done end)
+    return result
+  end, ...)
+end
+
 local T = new_set {
   hooks = {
     pre_case = function()
       child.restart { "-u", "scripts/minimal_init.lua", }
+      child.lua_func(function() M = require "async" end)
     end,
     post_once = child.stop,
   },
@@ -18,13 +35,33 @@ end
 
 T["await()"] = new_set()
 
-T["await()"]["returns resolved value"] = function() end
+T["await()"]["returns resolved value"] = function()
+  eq(await_value(42), { 42, })
+end
 
-T["await()"]["returns multiple resolved values"] = function() end
+T["await()"]["returns multiple resolved values"] = function()
+  eq(await_value(1, 2, 3), { 1, 2, 3, })
+end
 
-T["await()"]["errors when called outside coroutine"] = function() end
+T["await()"]["errors when called outside coroutine"] = function()
+  local result = child.lua_func(function()
+    return { pcall(M.await, function(resolve) resolve(42) end), }
+  end)
+  eq(result[1], false)
+  eq(result[2]:find "`await` can only be called in a coroutine" ~= nil, true)
+end
 
-T["await()"]["works inside async function"] = function() end
+T["await()"]["works inside async function"] = function()
+  eq(child.lua_func(function()
+    local result, done = {}, false
+    M.unwaited_async(function()
+      result = { M.await(function(resolve) resolve(42) end), }
+      done = true
+    end)()
+    vim.wait(10, function() return done end)
+    return result
+  end), { 42, })
+end
 
 T["unwaited_async()"] = new_set()
 
